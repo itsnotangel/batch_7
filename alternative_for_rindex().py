d4916fc2405d0e063e884a0c2fe50cb818620b8e
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-31 22:07:03 +0800

    Added a pseudocode.

--- alternative_for_rindex().py
+++ alternative_for_rindex().py
@@ -1,15 +1,5 @@
 # Program 10: Find the last occurrence index without using rindex()
 
 # Get input from user
-statement = input("Enter a statement: ")
-char_to_find = input("Enter the character to find: ")
-
 # Manually find the last occurrence index
-last_index = -1
-for i in range(len(statement) - 1, -1, -1):
-    if statement[i] == char_to_find:
-        last_index = i
-        break
-
 # Print the result
-print("Last occurrence index:", last_index)
